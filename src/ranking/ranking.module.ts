5d41f052a4067852bf2e69db9720f3e359c2570c
RgMellon <renan_gian@hotmail.com>
2024-09-18 07:42:48 -0300

    feat: creating ranking schema

--- src/ranking/ranking.module.ts
+++ src/ranking/ranking.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { RankingController } from './ranking.controller';
 import { RankingService } from './ranking.service';
+import { RankingSchema } from './interfaces/ranking.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Ranking', schema: RankingSchema }]),
+  ],
   controllers: [RankingController],
-  providers: [RankingService]
+  providers: [RankingService],
 })
 export class RankingModule {}
