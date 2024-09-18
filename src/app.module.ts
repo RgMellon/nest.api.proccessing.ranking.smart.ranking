a02b781f43f7cc5451b9868e8e19b0d5659d72c9
RgMellon <renan_gian@hotmail.com>
2024-09-18 07:30:36 -0300

    feat: creating ranking module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RankingModule } from './ranking/ranking.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+const connectionMoongoose =
+  'mongodb+srv://rgmelo94:qDcOSHhQrrtSdO6e@cluster0.q9qgq6n.mongodb.net/srchallenges?retryWrites=true&w=majority&appName=Cluster0';
 
 @Module({
-  imports: [],
+  imports: [
+    RankingModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(connectionMoongoose),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+
+const configService = new ConfigService();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: [
+        `amqp://${configService.get<string>('RABBITMQ_USER')}:${configService.get<string>('RABBITMQ_PASSWORD')}@${configService.get<string>('RABBITMQ_URL')}/${configService.get<string>('RABBITMQ_PROJECT_NAME')}`,
+      ],
+      noAck: false,
+      queue: 'ranking',
+    },
+  });
+
+  await app.listen();
 }
 bootstrap();
